3326d6e6283500ff2e367fbcf87f9fe93c295810
zerobit <orourke_kyle@icloud.com>
2020-08-22 17:23:20 -0400

    created unique id on new notes, required path in apiRoutes as well as new post method

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,6 +52,7 @@ const renderActiveNote = () => {
 // Get the note data from the inputs, save it to the db and update the view
 const handleNoteSave = function () {
   const newNote = {
+    id: Math.floor(Math.random()*1000),
     title: $noteTitle.val(),
     text: $noteText.val(),
   };
--- assets/routes/apiRoutes.js
+++ assets/routes/apiRoutes.js
@@ -1,7 +1,26 @@
-const noteDB = require("../db/db");
+const path = require("path");
+const noteDB = require("../db/db.json");
+const fs = require("fs");
 
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
         res.json(noteDB);
+    });
+
+    app.post("/api/notes", function(req, res){
+        fs.readFile("../db/db.json",(err,data)=>{
+            if(err) throw err;
+            let notes =JSON.parse(data);
+
+            notes.push(req.body);
+            const jsonString = JSON.stringify(notes);
+            fs.writeFile("../db/db.json", jsonString, (err)=> {
+                if (err){
+                    console.log("Write File ERROR", err);
+                }else{
+                    console.log("Write File SUCCESS");
+                }
+            });
+        });
     });
 };
