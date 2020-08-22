f335cc09e908153a58d09cc6ee12ba89b9acfaeb
zerobit <orourke_kyle@icloud.com>
2020-08-22 16:30:53 -0400

    added file type to paths in htmlRoutes so it would work

--- assets/routes/htmlRoutes.js
+++ assets/routes/htmlRoutes.js
@@ -3,9 +3,9 @@ const path = require("path");
 module.exports = function(app){
     // here: creates routes
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../html/notes"));
+        res.sendFile(path.join(__dirname, "../html/notes.html"));
     });
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../html/index"));
+        res.sendFile(path.join(__dirname, "../html/index.html"));
     });
 };
